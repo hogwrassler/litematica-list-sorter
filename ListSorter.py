f497b1ab480a211fc6cd609f3158e6820313a340
hog wrassler <hogwrassler@gmail.com>
2023-05-20 16:24:44 -0700

    add ListSorter.py, blockdata.json, .gitignore, and update README.md

---
+++ ListSorter.py
@@ -0,0 +1,67 @@
+import csv
+import json
+import math
+import sys 
+import ntpath
+import os
+
+if len(sys.argv) != 2:
+    print("Error: Incorrect number of arguments.\n" +
+    "Input a file path to a litematica material list file.\n" +
+    "Ex. python listSorter.py <path to minecraft>\.minecraft\config\litematica\material_list_###.txt")
+    sys.exit()
+
+if (not os.path.isfile(sys.argv[1])):
+    print("Error: File does not exist - " + sys.argv[1])
+    sys.exit()
+
+with open(sys.argv[1], newline='') as materialFile:
+    fieldnames = ['blank', 'item', 'count', 'available', 'missing', 'blank2']
+    wantedFieldNames = ['item','count']
+    dictReader = csv.DictReader(materialFile, fieldnames, delimiter='|')
+    itemList = list()
+    for dict in dictReader:
+        # create new dict only using item and count fields
+        newDict = {x:dict[x] for x in wantedFieldNames}
+        if (newDict["item"] is not None):
+            # strip whitespace from each dict item
+            strippedDict = { k:v.strip() for k, v in newDict.items()}
+            itemList.append(strippedDict)
+    # remove header and footer
+    itemList.pop(0)
+    itemList.pop(0)
+    itemList.pop(-1)
+        
+with open("blockdata.json", 'r') as f:
+    blocklist = json.load(f)
+
+# organize items by block family and calculate stack/shulker counts
+for item in itemList:
+    matchingBlocks = list(filter(lambda block: block['displayName'] == item['item'], blocklist))
+    if matchingBlocks:
+        item['stackSize'] = matchingBlocks[0]['stackSize']
+        item['family'] = matchingBlocks[0]['family']
+    else:
+        item['stackSize'] = 64
+        item['family'] = 'etc'
+        print("no match for " + item['item'])
+    stackCount = int(item['count']) / item['stackSize']
+    item['stacks'] = 0 if stackCount < 1 else math.ceil(stackCount)
+    shulkerCount = stackCount / 27
+    item['shulkers'] = 0 if shulkerCount < 1 else math.ceil(shulkerCount)
+
+sortedItemList = sorted(itemList, key=lambda x: x['family'])
+
+# create lists folder
+fileName = ntpath.basename(sys.argv[1]).replace('.txt', '_sorted.csv')
+path = os.path.join(os.getcwd(), 'lists')
+if not os.path.exists(path):
+    os.mkdir(path)
+
+# print out csv
+outputFile = os.path.join(path, fileName)
+print("Printing sorted csv to " + outputFile)
+with open(outputFile, 'w', newline='') as sortedCsv:
+    writer = csv.DictWriter(sortedCsv, fieldnames=['item', 'count', 'stacks', 'shulkers'], extrasaction='ignore')
+    writer.writeheader()
+    writer.writerows(sortedItemList)
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # litematica-list-sorter
+
+pass in a litematica material list and receive a sorted csv, grouped in order described below, with the total count, # stacks, # shulkers needed
+
+How to use:
+Must have python installed for use (https://www.python.org/downloads/)
+Open command prompt, navigate to this repo and enter:
+    python listSorter.py <path to litematica material list>
+
+The sorted list will be output to the lists folder within this repo as a .csv.
+
+Sort Order, alphabetically within:
+concrete
+terracotta
+wool
+glass
+acacia
+birch
+cherry
+crimson
+dark oak
+jungle
+mangrove
+oak
+spruce
+warped
+andesite
+basalt
+blackstone
+bricks
+calcite
+copper
+deepslate
+dripstone
+end
+granite
+mud
+mushroom
+nether bricks
+obsidian
+prismarine
+purpur
+quartz
+stone
+sand
+red sandstone
+redstone
+ores
+dirt
+plants
+iron
+lighting
+bee stuff
+sea stuff
+shulker
+cold stuff
+sculk
+amethyst
+job blocks, etc
